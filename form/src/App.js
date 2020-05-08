266eb0623c6a111cc587fa7836317d3f79486d61
IvanKhizhnyak <vanja.khizhnjak99@gmail.com>
2020-05-08 23:30:48 +0300

    Revert "Revert "added google map""
    This reverts commit 2bdc9130b11e3a1e5cd1079080d0db6f5feb379e.

--- form/src/App.js
+++ form/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import UserForm from './components/UserForm';
 import './UserForm.scss';
-
+import GoogleMapComponent from './components/GoogleMapComponent';
 function App() {
   return (
     <div className="App">
       <UserForm />
+      {/* <GoogleMapComponent /> */}
     </div>
   );
 }
---
+++ form/src/components/googleMap.js
@@ -0,0 +1,1 @@
+export const GOOGLE_API_KEY = 'AIzaSyB2R75dvMxekxyiz5ZDqJQPPmvZKojSN6k';
